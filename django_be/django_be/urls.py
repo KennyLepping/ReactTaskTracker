3996212a47d0ef2e09b980e27527d49868c7657a
Kenny Lepping <kennylepping@gmail.com>
2021-03-15 02:18:05 -0400

    Added the django rest framework so CRUD functionality now updates the DB

---
+++ django_be/django_be/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('tasks/', views.TaskList.as_view()),
+    path('tasks/<int:pk>/', views.TaskDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
